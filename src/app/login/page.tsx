ed226ee0bbe8d2bba32d677c91ba6237eb5b90bf
Pablo Silva <microgramxx@gmail.com>
2025-09-28 12:50:39 +0000

    nao vamos deixar essa pagina em fullwidth ..

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const loginImage = PlaceHolderImages.find((img) => img.id === 'login-bg');
 
   return (
-    <div className="w-full lg:grid lg:min-h-[calc(100vh-12rem)] lg:grid-cols-2">
+    <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
