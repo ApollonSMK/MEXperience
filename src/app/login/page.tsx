2ff6a4642c5fa9be400251b9a7aae34d099c80de
Pablo Silva <microgramxx@gmail.com>
2025-10-02 10:17:09 +0000

    meu eu adicionei agora o magicui porque voce continua recomendando shadc

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
 import { LoginForm } from '@/components/login-form';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
+import { Meteors } from '@/components/ui/meteors';
 
 export default function LoginPage() {
   const loginImage = PlaceHolderImages.find((img) => img.id === 'login-bg');
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-4xl">
+    <div className="relative bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10 overflow-hidden">
+      <Meteors number={30} />
+      <div className="w-full max-w-4xl z-10">
         <LoginForm image={loginImage} />
       </div>
     </div>
