199ee91cd104211d11f13e576745ed8b432ad64f
Pablo Silva <microgramxx@gmail.com>
2025-11-04 12:42:27 +0000

    Tenho esse erro quando faco upload de alguma imagem para o hero
    1/1
    
    N

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'supabase.me-experience.lu',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
