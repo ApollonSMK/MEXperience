d50c7b3c599081467e7804c00d86dd42babf7136
Pablo Silva <microgramxx@gmail.com>
2025-11-06 11:59:36 +0000

    continuamos com eror no deploy 2025-Nov-06 11:56:14.362625 Starting depl

--- src/lib/supabase/route-handler-client.ts
+++ src/lib/supabase/route-handler-client.ts
@@ -4,7 +4,7 @@
 import { createRouteHandlerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function getSupabaseRouteHandlerClient() {
+export async function getSupabaseRouteHandlerClient() {
   const cookieStore = cookies()
   return createRouteHandlerClient({
     cookies: () => cookieStore,
