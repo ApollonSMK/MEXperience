3cbb2b72afd69975a5868a539e9c8ede0e1f82a9
[dyad] <microgramxx@gmail.com>
2025-11-25 21:19:39 +0100

    [dyad] Adicionando bordas aos cartões - wrote 1 file(s)

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -40,7 +40,7 @@ export const BentoGridItem = ({
       whileHover={{ scale: 1.02 }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
       className={cn(
-        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-gray-200 justify-between flex flex-col space-y-4",
         className
       )}
     >
