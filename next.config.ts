554a312f8ce2b8aee585d82a5ed6f4a13e758598
[dyad] <microgramxx@gmail.com>
2025-11-27 12:48:34 +0100

    [dyad] Enable PWA testing in dev mode - wrote 4 file(s)

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  disable: false, // Agora ativado também em desenvolvimento para testares
   workboxOptions: {
     disableDevLogs: true,
   },
